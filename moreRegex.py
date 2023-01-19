2d8a725a980ca3227dc0da24b02342816ced5aac
lucasjotap <leiathemosthot@gmail.com>
2023-01-18 21:24:06 -0300

    Commit

--- moreRegex.py
+++ moreRegex.py
@@ -1,8 +1,15 @@
 import re 
 
-regex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d)')
-match_object = regex.findall('Cell: 454-545-5454 Work: 998-989-8989')
-match_object_two = regex.search('Cell: 454-545-5454 Work: 998-989-8989')
+regex_one = re.compile(r'\d\d\d-\d\d\d-\d\d\d')
+regex_two = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d)')
+regex_three = re.compile(r'\w\w\w\w\W')
 
-print(match_object)
+match_object_one = regex_one.findall('Cell: 454-545-5454 Work: 998-989-8989')
+match_object_two = regex_two.search('Cell: 454-545-5454 Work: 998-989-8989')
+match_object_three = regex_two.findall('Cell: 454-545-5454 Work: 998-989-8989')
+match_object_four = regex_three.findall('Cell: 454-545-5454 Work: 998-989-8989')
+
+print(match_object_one)
 print(match_object_two.group())
+print(match_object_three)
+print(match_object_four)
